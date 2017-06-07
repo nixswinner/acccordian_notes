2dfaf77d4065c75c4cf280e725f02fc7ae955787
nixswinner <nixswinner@gmail.com>
2017-06-08 01:15:05 +0300

    added the asset folder to store local html format data and added the navigate back functionality as well as the stylings

--- app/src/main/java/com/tergech/nixon/accordianview/MainActivity.java
+++ app/src/main/java/com/tergech/nixon/accordianview/MainActivity.java
@@ -21,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+
+
 
         // get the listview
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
@@ -48,8 +49,10 @@ public class MainActivity extends AppCompatActivity {
                         .show();
                 //passing data to the display activity
                 String data=listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition);
+                String topichead=listDataHeader.get(groupPosition);
                 Intent intent=new Intent(MainActivity.this,display.class);
                 intent.putExtra("data",data);
+                intent.putExtra("topichead",topichead);
                 startActivity(intent);
                 return false;
             }
@@ -63,37 +66,63 @@ public class MainActivity extends AppCompatActivity {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
+        listDataHeader.add("Topic 1");
+        listDataHeader.add("Topic 2");
+        listDataHeader.add("Topic 3");
+        listDataHeader.add("Topic 4");
+        listDataHeader.add("Topic 5");
+        listDataHeader.add("Topic 6");
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> Topic1 = new ArrayList<String>();
+        Topic1.add("SubTopic 1.1");
+        Topic1.add("SubTopic 1.2");
+        Topic1.add("SubTopic 1.3");
+        Topic1.add("SubTopic 1.4");
+        Topic1.add("SubTopic 1.5");
+        Topic1.add("SubTopic 1.6");
+        Topic1.add("SubTopic 1.7");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        List<String> Topic2 = new ArrayList<String>();
+        Topic2.add("SubTopic 2.1");
+        Topic2.add("SubTopic 2.2");
+        Topic2.add("SubTopic 2.3");
+        Topic2.add("SubTopic 2.4");
+        Topic2.add("SubTopic 2.5");
+        Topic2.add("SubTopic 2.6");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        List<String> Topic3 = new ArrayList<String>();
+        Topic3.add("SubTopic 3.1");
+        Topic3.add("SubTopic 3.2");
+        Topic3.add("SubTopic 3.3");
+        Topic3.add("SubTopic 3.4");
+        Topic3.add("SubTopic 3.5");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+        List<String> Topic4 = new ArrayList<String>();
+        Topic4.add("SubTopic 4.1");
+        Topic4.add("SubTopic 4.2");
+        Topic4.add("SubTopic 4.3");
+        Topic4.add("SubTopic 4.4");
+        Topic4.add("SubTopic 4.5");
+
+        List<String> Topic5 = new ArrayList<String>();
+        Topic5.add("SubTopic 5.1");
+        Topic5.add("SubTopic 5.2");
+        Topic5.add("SubTopic 5.3");
+        Topic5.add("SubTopic 5.4");
+        Topic5.add("SubTopic 5.5");
+
+        List<String> Topic6 = new ArrayList<String>();
+        Topic6.add("SubTopic 6.1");
+        Topic6.add("SubTopic 6.2");
+        Topic6.add("SubTopic 6.3");
+        Topic6.add("SubTopic 6.4");
+        Topic6.add("SubTopic 6.5");
+
+        listDataChild.put(listDataHeader.get(0), Topic1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), Topic2);
+        listDataChild.put(listDataHeader.get(2), Topic3);
+        listDataChild.put(listDataHeader.get(3), Topic4);
+        listDataChild.put(listDataHeader.get(4), Topic5);
+        listDataChild.put(listDataHeader.get(5), Topic6);
     }
 }
--- app/src/main/java/com/tergech/nixon/accordianview/display.java
+++ app/src/main/java/com/tergech/nixon/accordianview/display.java
@@ -3,6 +3,8 @@ package com.tergech.nixon.accordianview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 /**
@@ -11,19 +13,41 @@ import android.widget.TextView;
 
 public class display extends AppCompatActivity {
     TextView tvDisplay;
+    WebView webView;
+    String topichead;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
         tvDisplay=(TextView) findViewById(R.id.tvDisplay);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
+        webView=(WebView) findViewById(R.id.webview);
         //getting the data passed from the other activity
         Intent intent=getIntent();
         Bundle bundle=intent.getExtras();
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        topichead=bundle.getString("topichead");
+        this.setTitle(topichead);
+
         String data=bundle.getString("data");
+
         tvDisplay.setText(data);
+        webView.loadUrl(“file:///android_asset/help/help.html”);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                display.this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
 }
